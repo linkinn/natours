78dc669bcf6efdfdc1913a7f3340cc80089c066a
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2019-10-16 00:19:20 -0300

    validacao de erros

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const { resolve } = require('path');
 
 const app = express();
 
+const AppError = require('./utils/appError');
+
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -15,5 +19,11 @@ app.use(express.static(resolve(__dirname, '..', 'public')));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const dotenv = require('dotenv');
 const path = require('path');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 😡 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: path.resolve(__dirname, '..', 'config.env') });
 
 const app = require('./app');
@@ -16,6 +22,15 @@ mongoose
   .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLER REJECTION! 😡 Shutting down...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
