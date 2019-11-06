98c1d89bb23593a95c7a95f8db3236a9aab47241
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2019-11-05 22:48:10 -0300

    chore userRoute, tourRoute, reviewRoute adicionado protrecao e restrições

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const Tour = require('../../src/models/tourModel');
+const User = require('../../src/models/userModel');
+const Review = require('../../src/models/reviewModel');
 
 dotenv.config({ path: path.resolve(__dirname, '..', '..', 'config.env') });
 
@@ -18,11 +20,17 @@ mongoose
 
 // Read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // Import data into db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data import success');
   } catch (err) {
     console.log(err);
@@ -34,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data delete success');
   } catch (err) {
     console.log(err);
--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -5,11 +5,12 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -17,8 +18,14 @@ router
 
 router
   .route('/:id')
-  .get(authController.protect, reviewController.getReview)
-  .patch(authController.protect, reviewController.updateReview)
-  .delete(authController.protect, reviewController.deleteReview);
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
